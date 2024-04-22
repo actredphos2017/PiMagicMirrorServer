294a85784dff150f883f83e1ec45eaf2525f7bd3
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-04-22 20:15:07 +0800

    Optimize message pipeline

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import threading
 
 from modules.facial_recognition.main import main as facial_recognition
 from modules.voice_assistant.main import main as voice_assistant
-from pipe import Pipe
+from pipe import Pipe, Notification
 
 registered_modules = [
     voice_assistant,
     facial_recognition
 ]
 
+
+def logger(n: Notification):
+    print(n.stamp.strftime("%Y-%m-%d %H:%M:%S"), n.flag, n.msg)
+
+
 if __name__ == '__main__':
-    pipe = Pipe(lambda n: print(n.stamp.strftime("%Y-%m-%d %H:%M:%S"), n.flag, n.msg))
+    pipe = Pipe(logger)
     for module in registered_modules:
         threading.Thread(target=lambda: module(pipe)).start()
     pipe.hold()
--- pipe.py
+++ pipe.py
@@ -13,10 +13,7 @@ class Notification:
 class Subscriber:
     def __init__(self, flag: str, handler: Callable[[Notification], None]):
         self.flag = flag
-        self.handler = handler
-
-    def notify(self, notification: Notification):
-        self.handler(notification)
+        self.notify = handler
 
 
 class Pipe:
@@ -25,18 +22,21 @@ class Pipe:
         self.subscribers: list[Subscriber] = []
         self.logger = logger
 
-    def hold(self):
+    def hold(self) -> None:
         while True:
             notification: Notification = self.msgQueue.get()
             self.logger(notification)
             for subscriber in [s for s in self.subscribers if s.flag == notification.flag]:
-                subscriber.handler(notification)
+                subscriber.notify(notification)
 
-    def notify(self, notification: Notification):
+    def notify(self, notification: Notification) -> None:
         self.msgQueue.put(notification)
 
-    def send(self, flag: str, msg: any):
+    def send(self, flag: str, msg: any) -> None:
         self.notify(Notification(flag, msg))
 
-    def subscribe(self, listener: Subscriber):
-        self.subscribers.append(listener)
+    def subscribe(self, subscriber: Subscriber) -> bool:
+        if subscriber in self.subscribers:
+            return False
+        self.subscribers.append(subscriber)
+        return True
