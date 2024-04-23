27c87aead0f7dbf08c0159f17d9d5e27380766ac
David <daviddai278@gmail.com>
2024-04-23 14:32:54 +0800

    录音5s

--- modules/voice_assistant/main.py
+++ modules/voice_assistant/main.py
@@ -1,4 +1,5 @@
 import time
+import pyaudio, wave
 from datetime import datetime
 from typing import Callable
 
@@ -78,14 +79,43 @@ def usage():
     })
 
 
+def demo():
+    # 实例化一个PyAudio对象
+    pa = pyaudio.PyAudio()
+    # 打开声卡，设置 采样深度为16位、声道数为2、采样率为16、输入、采样点缓存数量为2048
+    stream = pa.open(format=pyaudio.paInt16, channels=2, rate=16000, input=True, frames_per_buffer=2048)
+    # 打开声卡，设置 采样深度为16位、声道数为2、采样率为16、输入、采样点缓存数量为2048
+    stream = pa.open(format=pyaudio.paInt16, channels=2, rate=16000, input=True, frames_per_buffer=2048)
+    # 新建一个列表，用来存储采样到的数据
+    record_buf = []
+    count = 0
+    while count < 8 * 5:
+        audio_data = stream.read(2048)  # 读出声卡缓冲区的音频数据
+        record_buf.append(audio_data)  # 将读出的音频数据追加到record_buf列表
+        count += 1
+        print('*')
+    wf = wave.open('01.wav', 'wb')  # 创建一个音频文件，名字为“01.wav"
+    wf.setnchannels(2)  # 设置声道数为2
+    wf.setsampwidth(2)  # 设置采样深度为
+    wf.setframerate(16000)  # 设置采样率为16000
+    # 将数据写入创建的音频文件
+    wf.writeframes("".encode().join(record_buf))
+    # 写完后将文件关闭
+    wf.close()
+    # 停止声卡
+    stream.stop_stream()
+    # 关闭声卡
+    stream.close()
+    # 终止pyaudio
+    pa.terminate()
+
 
 def main(pipe: Pipe):
     init_module(pipe)
     log('启动！')
-
+    demo()
     # 主代码从这里开始
-    while True:
-        time.sleep(3)
-
-        # 模拟魔镜输出
-        simulation_assistant()
+    # while True:
+    #     time.sleep(3)
+    #     模拟魔镜输出
+    # simulation_assistant()
