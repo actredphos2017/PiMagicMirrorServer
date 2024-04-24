bb010e4d59b333389cd96af14626dffa71af54c7
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-04-24 08:53:49 +0800

    天气信息存储，惰性更新

--- orm.py
+++ orm.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, JSON
+from sqlalchemy import create_engine, Column, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -17,6 +17,12 @@ class UserInfo(Base):
     nickname = Column(String, default="Guest")
     create_time = Column(DateTime, default=datetime.now)
     setting = Column(Text, default=json.dumps(CustomSetting.default().__dict__()))
+
+
+class KeyValuePairStorage(Base):
+    __tablename__ = "storage"
+    key = Column(String, primary_key=True)
+    value = Column(Text, nullable=True, default=None)
 
 
 Base.metadata.create_all(engine, checkfirst=True)
--- utils/caiyun_weather.py
+++ utils/caiyun_weather.py
@@ -1,4 +1,10 @@
+import json
+
 import requests
+
+from orm import Session, KeyValuePairStorage
+
+import datetime
 
 current_longitude = "120.35"
 current_latitude = "30.31"
@@ -10,9 +16,26 @@ def get_comprehensive_url(token: str, longitude, latitude):
 
 
 def request_for_comprehensive() -> dict | int:
+    with Session() as session:
+        old_weather: KeyValuePairStorage | None = session.query(KeyValuePairStorage).get("weather")
+        if old_weather:
+            old_data: dict = json.loads(old_weather.value)
+            if old_data['server_time'] + 900 > datetime.datetime.now().timestamp():
+                return old_data
+
     response = requests.get(get_comprehensive_url(caiyun_api_token, current_longitude, current_latitude))
     match response.status_code:
         case 200:
-            return dict(response.json())
+            respData = response.text
+            with Session() as session:
+                target_column = session \
+                    .query(KeyValuePairStorage) \
+                    .filter_by(key="weather")
+                if target_column.count() == 0:
+                    session.add(KeyValuePairStorage(key="weather", value=respData))
+                else:
+                    target_column.update({KeyValuePairStorage.value: respData})
+                session.commit()
+            return dict(json.loads(respData))
         case _:
             return response.status_code
