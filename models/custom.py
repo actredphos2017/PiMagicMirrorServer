39ef24b1559078457064e0b9fdb137af6d3fd847
Sakulin <actredphos2017@gmail.com>
2024-06-12 14:04:29 +0800

    添加：记事本与日程

--- models/custom.py
+++ models/custom.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 
 
@@ -50,13 +51,112 @@ class CustomComposeStructure:
         )
 
 
-class CustomSetting:
-    def __init__(self, compose_structure: CustomComposeStructure):
-        self.compose_structure = compose_structure
+class CustomSingleNote:
+    def __init__(self, content: str, create_time: datetime):
+        self.content = content
+        self.create_time = create_time
 
     def __dict__(self):
         return {
-            "compose_structure": self.compose_structure.__dict__()
+            "content": self.content,
+            "create_time": int(self.create_time.timestamp() * 1000)
+        }
+
+    @staticmethod
+    def from_dict(note: dict):
+        return CustomSingleNote(
+            content=note["content"],
+            create_time=datetime.fromtimestamp(float(note["create_time"]) / 1000)
+        )
+
+
+class CustomNote:
+    def __init__(self, notes: list[CustomSingleNote]):
+        self.notes = notes
+
+    def __dict__(self):
+        return {
+            "notes": [
+                note.__dict__() for note in self.notes
+            ],
+        }
+
+    @staticmethod
+    def from_dict(note: dict):
+        return CustomNote(
+            notes=note["notes"]
+        )
+
+    @staticmethod
+    def default():
+        return CustomNote(notes=[
+            CustomSingleNote(
+                content="在这里可以创建你的记事",
+                create_time=datetime.now()
+            )
+        ])
+
+
+class CustomSingleSchedule:
+    def __init__(self, name: str, date: datetime):
+        self.name = name
+        self.date = date
+
+    def __dict__(self):
+        return {
+            "name": self.name,
+            "date": int(self.date.timestamp() * 1000)
+        }
+
+    @staticmethod
+    def from_dict(schedule: dict):
+        return CustomSingleSchedule(
+            name=schedule["name"],
+            date=datetime.fromtimestamp(schedule["date"] / 1000),
+        )
+
+
+class CustomScheduleList:
+    def __init__(self, schedules: list[CustomSingleSchedule]):
+        self.schedules = schedules
+
+    @staticmethod
+    def default():
+        return CustomScheduleList(schedules=[
+            CustomSingleSchedule(
+                name="开会",
+                date=datetime.now()
+            )
+        ])
+
+    def __dict__(self):
+        return {
+            "schedules": [
+                schedule.__dict__() for schedule in self.schedules
+            ]
+        }
+
+    @staticmethod
+    def from_dict(schedule_list: dict):
+        return CustomScheduleList(schedule_list["schedules"])
+
+
+class CustomSetting:
+    def __init__(
+            self,
+            compose_structure: CustomComposeStructure,
+            note: CustomNote = CustomNote.default(),
+            schedule_list: CustomScheduleList = CustomScheduleList.default()
+    ):
+        self.compose_structure = compose_structure
+        self.note = note
+        self.schedule_list = schedule_list
+
+    def __dict__(self):
+        return {
+            "compose_structure": self.compose_structure.__dict__(),
+            "note": self.note.__dict__(),
+            "schedule_list": self.schedule_list.__dict__(),
         }
 
     @staticmethod
@@ -64,13 +164,21 @@ class CustomSetting:
         return CustomSetting(
             compose_structure=CustomComposeStructure.from_dict(
                 setting["compose_structure"]
-            )
+            ),
+            note=CustomNote.from_dict(
+                setting["note"]
+            ),
+            schedule_list=CustomScheduleList.from_dict(
+                setting["schedule_list"]
+            ),
         )
 
     @staticmethod
     def default():
         return CustomSetting(
-            compose_structure=CustomComposeStructure.default()
+            compose_structure=CustomComposeStructure.default(),
+            note=CustomNote.default(),
+            schedule_list=CustomScheduleList.default(),
         )
 
 
